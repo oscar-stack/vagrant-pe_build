ba8b94d9e5cd46e3c3a36c30469be36d76b03d39
Adrien Thebo <git@somethingsinistral.net>
2013-01-19 22:54:42 -0800

    Schedule puppet run post-classification for mco

--- bootstrap/base/provision/install_puppet_enterprise.sh
+++ bootstrap/base/provision/install_puppet_enterprise.sh
@@ -3,6 +3,10 @@ if [ -f /opt/puppet/bin/puppet ]; then
   echo "Skipping installation."
 else
   <%= @installer_cmd %>
-  echo " -- Performing Puppet run to preload classification"
-  /opt/puppet/bin/puppet agent -t
+  echo
+  echo
+  echo " -- Notice: scheduling Puppet run in one minute to install mcollective"
+  at next minute <<-AT
+  /opt/puppet/bin/puppet agent --onetime --noop
+  AT
 fi
