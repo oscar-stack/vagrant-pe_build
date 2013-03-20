7948b0a59f22902fe12caf267b54a1af1ecc7017
Adrien Thebo <git@somethingsinistral.net>
2013-03-19 17:14:54 -0700

    Print actual newlines instead of the string '\n'

--- lib/pe_build/command/list.rb
+++ lib/pe_build/command/list.rb
@@ -2,7 +2,7 @@ require 'vagrant'
 class PEBuild::Command::List < Vagrant::Command::Base
   def execute
     if File.directory? PEBuild.archive_directory and (entries = Dir["#{PEBuild.archive_directory}/*"])
-      puts entries.join('\n')
+      puts entries.join("\n")
     else
       warn "No PE versions downloaded."
     end
