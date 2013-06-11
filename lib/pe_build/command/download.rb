7c95ab06cdab4ea8571fdbbb9fbce5f51170d922
Adrien Thebo <git@somethingsinistral.net>
2013-06-10 22:41:12 -0700

    Basic implementation of pe-build download

--- lib/pe_build/command/download.rb
+++ lib/pe_build/command/download.rb
@@ -1,11 +1,37 @@
 require 'vagrant'
+require 'optparse'
+require 'pe_build/archive'
 
 module PEBuild
 class Command
 class Download < Vagrant.plugin(2, :command)
+
   def execute
-    raise NotImplementedError
-    #@env.action_runner.run(:download_pe_build)
+
+    options = {}
+
+    parser = OptionParser.new do |o|
+      o.banner = "Usage: vagrant pe_build download --version <version> --dir <dir>"
+      o.separator ''
+
+      o.on('-v', '--version=val', String, "The version of PE to fetch") do |val|
+        options[:version] = val
+      end
+
+      o.on('-d', '--dir=val', String, 'The URL basedir containing the file') do |val|
+        options[:dir] = val
+      end
+    end
+
+    argv = parse_options(parser)
+    filename = argv.last
+
+    unless options[:version]
+      raise Vagrant::Errors::CLIInvalidUsage, :help => parser.help.chomp
+    end
+
+    archive = PEBuild::Archive.new(filename, options[:version], @env.ui)
+    archive.download_from(options[:dir])
   end
 end
 end
