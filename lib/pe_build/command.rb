5842c2e2a0120aaa444376ddc2251eb2d62d3fc6
Adrien Thebo <git@somethingsinistral.net>
2013-06-12 20:04:56 -0700

    Add help output for vagrant pe-build

--- lib/pe_build/command.rb
+++ lib/pe_build/command.rb
@@ -19,7 +19,7 @@ class Command < Vagrant.plugin(2, :command)
     elsif @subcommand
       raise "Unrecognized subcommand #{@subcommand}"
     else
-      PEBuild::Command::List.new(@argv, @env).execute
+      print_help
     end
   end
 
@@ -40,6 +40,29 @@ class Command < Vagrant.plugin(2, :command)
       require_relative 'command/list'
       PEBuild::Command::List
     end
+  end
+
+  def print_help
+    cmd = 'vagrant pe-build'
+    opts = OptionParser.new do |opts|
+      opts.banner = "Usage: #{cmd} <command> [<args>]"
+      opts.separator ""
+      opts.separator "Available subcommands:"
+
+      # Add the available subcommands as separators in order to print them
+      # out as well.
+      keys = []
+      @subcommands.each { |key, value| keys << key.to_s }
+
+      keys.sort.each do |key|
+        opts.separator "     #{key}"
+      end
+
+      opts.separator ""
+      opts.separator "For help on any individual command run `#{cmd} COMMAND -h`"
+    end
+
+    @env.ui.info(opts.help, :prefix => false)
   end
 end
 end
