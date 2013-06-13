cc1e4418fd82c023b683da05e2221737f0ca4404
Adrien Thebo <git@somethingsinistral.net>
2013-06-12 21:12:01 -0700

    Add progress bar to archive downloading

--- lib/pe_build/archive.rb
+++ lib/pe_build/archive.rb
@@ -1,6 +1,10 @@
 require 'pe_build'
 require 'fileutils'
+
 require 'tempfile'
+require 'open-uri'
+require 'progressbar'
+
 
 module PEBuild
 class Archive
@@ -41,6 +45,8 @@ class Archive
     end
   end
 
+  HEADERS = {'User-Agent' => "Vagrant/PEBuild (v#{PEBuild::VERSION})"}
+
   # @todo Download to tempfile, and move into place when download complete
   def download_from(download_dir)
 
@@ -49,12 +55,12 @@ class Archive
     else
       prepare_for_copy!
 
-      url = "#{download_dir}/#{@filename}"
-      url.gsub!(':version', @version)
+      str = "#{download_dir}/#{@filename}"
+      str.gsub!(':version', @version)
 
-      cmd = %{curl --fail -L -A "Vagrant/PEBuild (v#{PEBuild::VERSION})" -O #{url}}
-      @ui.info "Executing '#{cmd}'"
-      Dir.chdir(PEBuild.archive_directory) { %x{#{cmd}} }
+      tmpfile = open_uri(str)
+
+      FileUtils.mv tmpfile, @archive_path
     end
   end
 
@@ -74,6 +80,29 @@ class Archive
     FileUtils.mv tmpfile, filename
   ensure
     tmpfile.unlink
+  end
+
+  def open_uri(str)
+    uri = URI.parse(str)
+    progress = nil
+
+    content_length_proc = lambda do |length|
+      if length and length > 0
+        progress = ProgressBar.new(@version, length)
+        progress.file_transfer_mode
+      end
+    end
+
+    progress_proc = lambda do |size|
+      progress.set(size) if progress
+    end
+
+    options = HEADERS.merge({
+      :content_length_proc => content_length_proc,
+      :progress_proc       => progress_proc,
+    })
+
+    uri.open(options)
   end
 end
 end
