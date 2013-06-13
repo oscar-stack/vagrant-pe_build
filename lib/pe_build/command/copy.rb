40105432c3575113bb8c7db1bd2317e097c8d07a
Adrien Thebo <git@somethingsinistral.net>
2013-06-12 20:06:57 -0700

    Actually check in copy command

---
+++ lib/pe_build/command/copy.rb
@@ -0,0 +1,38 @@
+require 'vagrant'
+require 'optparse'
+require 'pe_build/archive'
+
+module PEBuild
+class Command
+class Copy < Vagrant.plugin(2, :command)
+
+  def execute
+
+    options = {}
+
+    parser = OptionParser.new do |o|
+      o.banner = "Usage: vagrant pe-build copy path/to/installer.tar.gz"
+      o.separator ''
+
+      o.on('-v', '--version=val', String, "The version of PE to fetch") do |val|
+        options[:version] = val
+      end
+    end
+
+    argv = parse_options(parser)
+    fpath = argv.last
+
+    basename = File.basename(fpath)
+    dirname  = File.dirname(fpath)
+
+    #unless options[:version]
+    #  raise Vagrant::Errors::CLIInvalidUsage, :help => parser.help.chomp
+    #end
+
+    archive = PEBuild::Archive.new(fpath, '2.7.2', @env.ui)
+    archive.copy_from(dirname)
+  end
+end
+end
+end
+
