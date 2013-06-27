a1fdaf6b9979e96f7ff519479cbeea70a5edeb35
Thomas Linkin <trlinkin@gmail.com>
2013-06-27 09:36:07 -0400

    Add config util module
    This commit adds a config util module. This module is meant to help do
    something that the current Vagrant plugin config implimentation does not
    do. In this commit, the module only contains one method to merge config
    classes.

---
+++ lib/pe_build/util/config.rb
@@ -0,0 +1,29 @@
+module PEBuild
+module Util
+module Config
+
+  # Merge configuration classes together with the "local" object overwriting any
+  # values set in the "other" object. This uses the default merging in the Vagrant
+  # plugin config class. The builtin merge function is not straight forward however.
+  # It needs to be called on the object being overwritten. When using a subclass of
+  # a global config for a provisioner config, the builtin merge method cannot actually
+  # merge them in the direction that would be needed.
+  #
+  # This function assumes that the "local" object is of the same class or at the very
+  # least a subclass of "other".
+  #
+  # @param [Object] Local configuration class to merge
+  # @param [Object] Other configuration class to merge
+  # @return [Object] New object of the same class as Local that represents the merged result
+  #
+  def self.local_merge(local,other)
+    if other.class >= local.class
+      result = local.class.new
+
+      result = result.merge(other)
+      result = result.merge(local)
+    end
+  end
+end
+end
+end
