72fc73ca1f4d485cf36e2910639e554824f6d2fc
Adrien Thebo <git@somethingsinistral.net>
2013-06-29 17:07:57 -0700

    Implement naive tar unpacker.

--- lib/pe_build/archive.rb
+++ lib/pe_build/archive.rb
@@ -4,6 +4,8 @@ require 'pe_build/idempotent'
 require 'pe_build/transfer/file'
 require 'pe_build/transfer/uri'
 
+require 'pe_build/unpack/tar'
+
 require 'fileutils'
 
 module PEBuild
@@ -31,7 +33,8 @@ class Archive
 
   # @param fs_dir [String] The base directory to extract the installer to
   def unpack_to(fs_dir)
-    raise NotImplementedError
+    tar = PEBuild::Unpack::Tar.new(archive_path, fs_dir)
+    tar.unpack
   end
 
   # @param fs_dir [String] The base directory holding the archive
---
+++ lib/pe_build/unpack/tar.rb
@@ -0,0 +1,18 @@
+require 'minitar'
+
+module PEBuild
+class Archive
+class Unpacker
+
+  # @param src [String]
+  # @param dst [String]
+  def initialize(src, dst)
+    @src, @dst = src, dst
+  end
+
+  def unpack
+    Archive::Tar::Minitar.unpack(@src, @dst)
+  end
+end
+end
+end
