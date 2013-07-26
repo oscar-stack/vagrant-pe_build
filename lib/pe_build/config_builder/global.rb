f4e293b4a2eb5373b3f4590a1d740012fc937de7
Adrien Thebo <git@somethingsinistral.net>
2013-07-26 10:51:45 -0700

    Implement config_builder/global model

--- lib/pe_build/config_builder/global.rb
+++ lib/pe_build/config_builder/global.rb
@@ -4,7 +4,28 @@ module PEBuild
   module ConfigBuilder
     class Global < ::ConfigBuilder::Model::Base
 
-      ::ConfigBuilder::ModelCollection.provisioner.register('global', self)
+    # @!attribute [rw] download_root
+    attr_accessor :download_root
+
+    # @!attribute [rw] version
+    attr_accessor :version
+
+    # @!attribute [rw] suffix
+    attr_accessor :suffix
+
+    # @!attribute [rw] filename
+    attr_accessor :filename
+
+    def to_proc
+      Proc.new do |global_config|
+        global_config.pe_build.download_root = @download_root if defined? @download_root
+        global_config.pe_build.version       = @version       if defined? @version
+        global_config.pe_build.suffix        = @suffix        if defined? @suffix
+        global_config.pe_build.filename      = @filename      if defined? @filename
+      end
+    end
+
+    ::ConfigBuilder::ModelCollection.provisioner.register('global', self)
     end
   end
 end
