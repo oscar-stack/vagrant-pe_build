bd838c927abf4b47cf8a13921ded74337efe245d
Adrien Thebo <git@somethingsinistral.net>
2013-08-11 01:31:01 -0700

    Implement posix run_install capability

--- lib/pe_build/cap/run_install/posix.rb
+++ lib/pe_build/cap/run_install/posix.rb
@@ -1,5 +1,26 @@
+require 'pe_build/on_machine'
 class PEBuild::Cap::RunInstall::POSIX
-  def self.run_install(machine)
 
+  extend PEBuild::OnMachine
+
+  def self.run_install(machine, config)
+
+    root = File.join('/vagrant', PEBuild::WORK_DIR)
+
+    cmd_path = []
+    cmd_path << root
+
+    cmd_path << "puppet-enterprise-#{config.version}-#{config.suffix}"
+    cmd_path << "puppet-enterprise-installer"
+
+    cmd     = File.join(cmd_path)
+    answers = File.join(root, 'answers', "#{machine.name}.txt")
+
+    argv = "#{cmd} -a #{answers}"
+
+    on_machine(machine, argv)
+
+    machine.ui.info I18n.t('pe_build.provisioner.pe_bootstrap.scheduling_run')
+    machine.communicate.sudo("echo '/opt/puppet/bin/puppet agent -t' | at next minute")
   end
 end
