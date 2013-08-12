0f383d9be5aa4290ed0fa094f9e44de09333081d
Adrien Thebo <git@somethingsinistral.net>
2013-08-12 16:30:47 -0700

    Add factory method for pe_build/transfer classes

--- lib/pe_build/transfer.rb
+++ lib/pe_build/transfer.rb
@@ -2,6 +2,22 @@ module PEBuild
   module Transfer
     require 'pe_build/transfer/http'
     require 'pe_build/transfer/file'
+
+    IMPLEMENTATIONS = {
+      'http'  => PEBuild::Transfer::HTTP,
+      'https' => PEBuild::Transfer::HTTP,
+      'file'  => PEBuild::Transfer::File,
+    }
+
+    def self.generate(src, dst)
+      schema = src.schema
+
+      if (klass = IMPLEMENTATION[schema])
+        klass.new(src, dst)
+      else
+        raise "Schema #{schema.inspect} cannot be handled by any file transferrers"
+      end
+    end
   end
 end
 
