da313b30acf94260157789ff5d4d99242192a355
Adrien Thebo <git@somethingsinistral.net>
2013-09-12 14:19:41 -0700

    [transfer/open_uri] rescue StandardError on download fail
    There are too many possible exceptions that can be raised while trying
    to download a file, from OpenURI::HTTPError, OpenSSL::SSL::SSLError,
    SocketError, to Errno::ENOSPC. Instead of playing exception bingo, we
    rescue all descendants of StandardError, all of which should hopefully
    be things we can handle _somewhat_ gracefully.

--- lib/pe_build/transfer/open_uri.rb
+++ lib/pe_build/transfer/open_uri.rb
@@ -25,7 +25,7 @@ class PEBuild::Transfer::OpenURI
       tmpfile = download_file
       FileUtils.mv(tmpfile, @dst)
     end
-  rescue ::OpenURI::HTTPError, ::OpenSSL::SSL::SSLError, ::SocketError => e
+  rescue StandardError => e
     raise DownloadFailed, :uri => @uri, :msg => e.message
   end
 
