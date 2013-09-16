ef53823b6eedc2ebfc09df20ae559f3adb2844d1
Reid Vandewiele <reid@puppetlabs.com>
2013-09-16 16:57:31 -0700

    Remove redundant logic in windows detect_installer
    The stubs will probably never be used due to possible future API
    changes, so no point having them. If they're ACTUALLY needed later it's
    easy enough to build them out again.

--- lib/pe_build/cap/detect_installer/windows.rb
+++ lib/pe_build/cap/detect_installer/windows.rb
@@ -5,28 +5,9 @@
 # @protected
 class PEBuild::Cap::DetectInstaller::Windows < PEBuild::Cap::DetectInstaller::Base
 
-  # @!method supported_releases
-  #   @abstract
-  #   @return [Array<String>] All supported releases for the distribution
-
   def detect
-    unless supported_releases.include? dist_version
-      raise PEBuild::Cap::DetectInstaller::DetectFailed,
-        :name  => @machine.name,
-        :error => "#{self.class.name} release #{dist_version} not supported"
-    end
-
+    # Yes, it really is this simple. For Windows anyway.
     "puppet-enterprise-#{@version}.msi"
-  end
-
-  private
-
-  def dist_version
-    'not implemented'
-  end
-
-  def supported_releases
-    ['not implemented']
   end
 
 end
