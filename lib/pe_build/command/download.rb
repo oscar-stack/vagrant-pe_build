2594cf44f9d8897ad13267bbb95dc822260a20f6
Adrien Thebo <git@somethingsinistral.net>
2013-12-06 13:09:12 -0800

    [command/download] deprecate download, delegate to copy

--- lib/pe_build/command/download.rb
+++ lib/pe_build/command/download.rb
@@ -1,35 +1,10 @@
 require 'pe_build/archive'
+require 'pe_build/command/copy'
 
 class PEBuild::Command::Download < Vagrant.plugin(2, :command)
 
   def execute
-
-    options = {}
-
-    parser = OptionParser.new do |o|
-      o.banner = "Usage: vagrant pe-build download --version <version> --dir <dir>"
-      o.separator ''
-
-      o.on('-v', '--version=val', String, "The version of PE to fetch") do |val|
-        options[:version] = val
-      end
-
-      o.on('-d', '--dir=val', String, 'The URL basedir containing the file') do |val|
-        options[:dir] = val
-      end
-    end
-
-    argv = parse_options(parser)
-    filename = argv.last
-
-    unless options[:version]
-      raise Vagrant::Errors::CLIInvalidUsage, :help => parser.help.chomp
-    end
-
-    uri = URI.parse(options[:dir])
-
-    archive = PEBuild::Archive.new(filename, @env)
-    archive.version = options[:version]
-    archive.fetch(options[:dir])
+    @env.ui.warn "vagrant pe-build download is deprecated, use vagrant pe-build copy", :prefix => true
+    PEBuild::Command::Copy.new(@argv, @env).execute
   end
 end
