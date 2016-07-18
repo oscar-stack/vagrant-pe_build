oscar-stack/vagrant-pe_build

commit dfedd3fdc4864040babcdc8526df005e0d4320bc

Update expired GPG keys during installation
The GPG key bundled with most PE packages prior to 2016.2.0 expired on
July 8th, 2016. This causes installation to fail on Debian-based systems.
This patch resolves the issue by downloading the latest GPG key from
https://apt.puppetlabs.com/ and swapping it into the unpacked installer
before installation. Post installation, the GPG key bundled with the
`pe_module` is also updated as it suffers from the same issue.

Fixes oscar-stack/vagrant-pe_build#120.

lib/pe_build/provisioner/pe_bootstrap.rb
@@ -186,10 +186,10 @@ module PEBuild
         else
           if @config.shared_installer
             root = File.join('/vagrant', PEBuild::WORK_DIR)
-            installer_path = File.join(root, @archive.installer_dir, 'puppet-enterprise-installer')
+            installer_path = File.join(root, @archive.installer_dir)
             answers = File.join(root, 'answers', "#{machine.name}.txt")
           else
-            installer_path = File.join(@archive.installer_dir, 'puppet-enterprise-installer')
+            installer_path = File.join(root, @archive.installer_dir)
             answers = File.join("#{machine.name}.txt")
           end
 

lib/pe_build/cap/run_install/posix.rb
@@ -10,16 +10,35 @@ class PEBuild::Cap::RunInstall::POSIX
   # @param installer_dir [String] A path to the directory where PE installers
   #   are kept.
   # @param answers [String] A path to a file containing installation answers.
-  # @option options [Boolean] A flag which controls whether the PEM installer
-  #   introduced in 2016.2 should be used.
+  #
+  # @param options [Hash] Additional options that influence installer behavior.
+  # @option options [Boolean] :use_pem A flag which controls whether the PEM
+  #   installer introduced in 2016.2 should be used.
   #
   # @return [void]
   def self.run_install(machine, installer_path, answers, **options)
+
+    # Update GPG key used by pre-2016.2 installers.
+    on_machine(machine, <<-EOS)
+if [ -e #{installer_path}/gpg ]; then
+  curl https://apt.puppetlabs.com/pubkey.gpg > #{installer_path}/gpg/GPG-KEY-puppetlabs
+fi
+EOS
+
     if options.fetch(:use_pem, false)
-      on_machine(machine, "#{installer_path} -c #{answers}")
+      on_machine(machine, "#{installer_path}/puppet-enterprise-installer -c #{answers}")
     else
-      on_machine(machine, "#{installer_path} -a #{answers}")
+      on_machine(machine, "#{installer_path}/puppet-enterprise-installer -a #{answers}")
     end
+
+    # Update GPG key used by pe_repo.
+    on_machine(machine, <<-EOS)
+if [ -e /opt/puppetlabs/puppet/modules/pe_repo ]; then
+  cp #{installer_path}/gpg/GPG-KEY-puppetlabs /opt/puppetlabs/puppet/modules/pe_repo/files/GPG-KEY-puppetlabs
+elif [ -e /opt/puppet/share/puppet/modules/pe_repo ]; then
+  cp #{installer_path}/gpg/GPG-KEY-puppetlabs /opt/puppet/share/puppet/modules/pe_repo/files/GPG-KEY-puppetlabs
+fi
+EOS
 
     if machine.communicate.test('which at')
       machine.ui.info I18n.t('pebuild.cap.run_install.scheduling_run')

lib/pe_build/cap/run_install/windows.rb
@@ -11,6 +11,8 @@ class PEBuild::Cap::RunInstall::Windows
   # @param answers [Hash[String => String}] A hash of options that will be
   #   passed to msiexec as `key=value` pairs.
   #
+  # @param options [Hash] Additional options that influence installer behavior.
+  #
   # @return [void]
   def self.run_install(machine, installer_path, answers, **options)
     install_options = answers.map{|e| e.join('=')}.join(' ')

commit e540b8c594139d6ef2ccfe3d0f38df522908ad9a

Fix installer_path used for remote installs
This patch fixes a bug introduced in commit dfedd3f, which fixed the expired
Puppet Labs GPG key used to sign packages. The GPG fix accidentally copied the
installer path used when `shared_installer = true` into the code path for
`shared_installer = false`. This resulted in a "no implicit conversion of nil
into String" error during installation on remote systems.

Fixes oscar-stack/vagrant-pe_build#123.

lib/pe_build/provisioner/pe_bootstrap.rb
@@ -189,7 +189,7 @@ module PEBuild
             installer_path = File.join(root, @archive.installer_dir)
             answers = File.join(root, 'answers', "#{machine.name}.txt")
           else
-            installer_path = File.join(root, @archive.installer_dir)
+            installer_path = @archive.installer_dir
             answers = File.join("#{machine.name}.txt")
           end
 