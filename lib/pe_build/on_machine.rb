bd4115fa73370e458526621832eb8f7d4f3114b9
Charlie Sharpsteen <chuck@puppet.com>
2016-09-10 13:38:52 -0700

    Allow options to be passed to on_machine
    This commit updates the `on_machine` utility method to accept options
    which can be passed to the communicator. The method invoked on the
    communicator is changed from `sudo` to `execute`, but the `:sudo` option
    is set to `true` by default.

--- lib/pe_build/on_machine.rb
+++ lib/pe_build/on_machine.rb
@@ -1,7 +1,24 @@
 module PEBuild
   module OnMachine
-    def on_machine(machine, cmd)
-      machine.communicate.sudo(cmd) do |type, data|
+    # Execute a command on a machine and log output
+    #
+    # This method invokes the `execute` method of the machine's communicator
+    # and logs any resulting output at info level.
+    #
+    #
+    # @param machine [Vagrant::Machine] The Vagrant machine on which to run the
+    #   command.
+    # @param cmd [String] The command to run.
+    # @param options [Hash] Additional options to pass to the `execute` method
+    #   of the communicator.
+    # @option options [Boolean] :sudo A flag which controls whether the command
+    #   is executed with elevated privilages. Defaults to `true`.
+    #
+    # @return [void]
+    def on_machine(machine, cmd, **options)
+      options[:sudo] = true unless options.has_key?(:sudo)
+
+      machine.communicate.execute(cmd, options) do |type, data|
         color = (type == :stdout) ? :green : :red
         machine.ui.info(data.chomp, :color => color, :prefix => true)
       end
