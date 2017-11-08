9e0a5233105b9a8d96722426d349f227d3ccc0fb
Charlie Sharpsteen <chuck@puppet.com>
2017-11-07 16:53:47 -0800

    Add support for 2016.4.9

--- lib/pe_build/release/2016_4.rb
+++ lib/pe_build/release/2016_4.rb
@@ -35,4 +35,5 @@ module PEBuild::Release
   @releases['2016.4.6'] = twentysixteen_four_x
   @releases['2016.4.7'] = twentysixteen_four_x
   @releases['2016.4.8'] = twentysixteen_four_x
+  @releases['2016.4.9'] = twentysixteen_four_x
 end
