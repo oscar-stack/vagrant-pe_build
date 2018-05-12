cf2397f1d99fe50673d9252d2972b557680ff2a6
Charlie Sharpsteen <chuck@puppet.com>
2018-05-11 19:13:27 -0700

    Update CLI acceptance test to use CentOS 7

--- acceptance/pe_build/pe_build_cli_spec.rb
+++ acceptance/pe_build/pe_build_cli_spec.rb
@@ -7,7 +7,7 @@ module Vagrant
     end
 
     OutputTester[:pe_available] = lambda do |text|
-      text =~ /puppet-enterprise-\d+\.\d\.\d-el-6-x86_64\.tar\.gz/
+      text =~ /puppet-enterprise-\d+\.\d\.\d-el-7-x86_64\.tar\.gz/
     end
   end
 end
@@ -35,7 +35,7 @@ describe 'vagrant CLI: pe-build', component: 'cli/pe-build' do
 
     result = execute('vagrant', 'pe-build', 'copy',
       "--release=#{options[:pe_latest]}",
-      "#{download_url}/puppet-enterprise-:version-el-6-x86_64.tar.gz")
+      "#{download_url}/puppet-enterprise-:version-el-7-x86_64.tar.gz")
     expect(result).to exit_with(0)
 
     result = execute('vagrant', 'pe-build', 'list')
