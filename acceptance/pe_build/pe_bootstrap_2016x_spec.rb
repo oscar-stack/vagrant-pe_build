90e92442e7315937837e723fc38905c0b82fef19
Charlie Sharpsteen <chuck@puppet.com>
2018-05-22 09:10:28 -0500

    Remove 2016.latest acceptance tests

--- acceptance/pe_build/pe_bootstrap_2016x_spec.rb
+++ acceptance/pe_build/pe_bootstrap_2016x_spec.rb
@@ -62,35 +62,4 @@ shared_examples 'provider/provisioner/pe_bootstrap/2016x' do |provider, options|
       expect(result.stderr).to match(/Could not find a certificate/)
     end
   end
-
-  context 'when installing PE 2016.latest' do
-    it 'provisions masters with pe_bootstrap and agents with pe_agent' do
-      status('Test: pe_bootstrap master install')
-      assert_execute('vagrant', 'up', "--provider=#{provider}", 'pe-2016latest-master')
-
-      status('Test: pe_bootstrap master running after install')
-      result = execute('vagrant', 'ssh',
-        'pe-2016latest-master',
-        '-c', 'sudo /opt/puppetlabs/bin/puppet status --terminus=rest')
-      expect(result).to exit_with(0)
-      expect(result.stdout).to match('"is_alive": true')
-
-      status('Test: pe_agent install')
-      result = assert_execute('vagrant', 'up', "--provider=#{provider}", 'pe-2016latest-agent')
-
-      status('Test: pe_agent signed cert during install')
-      result = execute('vagrant', 'ssh',
-        'pe-2016latest-master',
-        '-c', 'sudo /opt/puppetlabs/bin/puppet cert list pe-2016latest-agent.pe-bootstrap.vlan')
-      expect(result).to exit_with(0)
-
-      status('Test: pe_agent cert purged when vm destroyed')
-      result = assert_execute('vagrant', 'destroy', '-f', 'pe-2016latest-agent')
-      result = execute('vagrant', 'ssh',
-        'pe-2016latest-master',
-        '-c', 'sudo /opt/puppetlabs/bin/puppet cert list pe-2016latest-agent.pe-bootstrap.vlan')
-      expect(result.stderr).to match(/Could not find a certificate/)
-    end
-  end
-
 end
